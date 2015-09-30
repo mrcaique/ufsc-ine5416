772e0ef2a401d9cc50eb8709d0a100eeb14c22e6
CaiqueMarques <caiquermarques95@gmail.com>
2015-09-29 23:07:42 -0300

    Update report 3

---
+++ relatorios/relatorio3/eratosthenes.py
@@ -0,0 +1,23 @@
+from sys import argv
+from math import sqrt
+
+# Sieve of Eratosthenes
+# A simple algorithm that returns a list of
+# primes to a given number
+
+# Given a number, verify if it is a prime number
+def sieve(number):
+	limit = int(sqrt(number))
+	for i in range(2, limit+1, 1):
+		if (number % i == 0):
+			return False
+	return True
+
+# main function
+max_seq = int(argv[1])
+print(2, end=' ')
+for i in range(3, max_seq, 2):
+	if (sieve(i)):
+		print(i, end=' ')
+
+print("\n")
