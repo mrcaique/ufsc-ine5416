932eafc2ebdd26648989cd0c2f64ba6bfdb158ae
CaiqueMarques <caiquermarques95@gmail.com>
2015-09-29 23:07:05 -0300

    Update to Python 3

--- relatorios/relatorio3/apod.py
+++ relatorios/relatorio3/apod.py
@@ -1,7 +1,7 @@
-import json
-import urllib
+from json import loads
+from urllib.request import urlopen
 
-# Astronomy Picture of the Day API powered by Nasa's Data Portal
+# Astronomy Picture of the Day API powered by NASA's Data Portal
 # More information in: https://data.nasa.gov/developer
 
 # main function
@@ -9,9 +9,10 @@ import urllib
 # the url of the image and, by end, a few explanation about the image
 # written by an astronomer.
 url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
-data = json.loads(urllib.urlopen(url).read())
+string_request = str(urlopen(url).read(), encoding='ascii')
+data = loads(string_request)
 
-print "ASTRONOMY PICTURE OF THE DAY"
-print data['title']
-print data['url']
-print data['explanation']
+print("ASTRONOMY PICTURE OF THE DAY")
+print(data['title'])
+print(data['url'])
+print(data['explanation'])
--- relatorios/relatorio3/bitcoin.py
+++ relatorios/relatorio3/bitcoin.py
@@ -1,30 +1,36 @@
-import sys
-import json
-import urllib
+from sys import argv
+from urllib.request import urlopen
+from json import loads
+from urllib.error import URLError
 
 # Bitcoin Price Index API powered by CoinDesk. 
 # More information avaiable in: http://www.coindesk.com/api/
 
 # Shows the list of currencies which the price of Bitcoin can be seen.
 def help():
-	data = json.loads(urllib.urlopen('http://api.coindesk.com/v1/bpi/supported-currencies.json').read())
+	url = 'http://api.coindesk.com/v1/bpi/supported-currencies.json'
+	string_request = str(urlopen(url).read(), encoding='utf-8')
+	data = loads(string_request)
 	for content in data:
-		print "%(currency)s - %(country)s" % content
-	print "\nType:"
-	print "\t" + 'python bitcoin.py [currency1] [currency2] [currency3] ...' 
-	print "to view the price of Bitcoin in the respective currency."
+		print("%(currency)s - %(country)s" % content)
+	print("\nType:")
+	print("\t" + 'python3 bitcoin.py [currency1] [currency2] [currency3] ...')
+	print("to view the price of Bitcoin in the respective currency.")
 
-# Shows the price of the Bitcoin in the currency referred in the "currency" parameter
+# Shows the price of the Bitcoin in the currency referred in the "currency" parameter.
 def get_country(currency):
+	url = 'https://api.coindesk.com/v1/bpi/currentprice/' + currency + '.json'
+	
 	try:
-		data = json.loads(urllib.urlopen('https://api.coindesk.com/v1/bpi/currentprice/' 
-			+ currency + '.json').read())
-	except ValueError as e:
-		print "\t" + "Currency " + currency + " not found." 
-		print "\t" + "Type 'python bitcoin.py help' to view the list of currencies."
+		string_request = str(urlopen(url).read(), encoding='utf-8')
+	except URLError as e:
+		print("\t" + "Currency " + currency + " not found.")
+		print("\t" + "Type 'python3 bitcoin.py help' to view the list of currencies.")
 		return
+	
+	data = loads(string_request)
 	price = data['bpi'][currency]
-	print "[" + price['code'] + "]" + " $" + price['rate'] + " - " + price['description']
+	print("[" + price['code'] + "]" + " $" + price['rate'] + " - " + price['description'])
 
 # Shows the price of the Bitcoin in the US Dollar, GB Pound and Euro currencies.
 def default():
@@ -32,20 +38,26 @@ def default():
 	priceGBP = data['bpi']['GBP']
 	priceEUR = data['bpi']['EUR']
 
-	print "[" + priceUS['code'] + "] " + priceUS['symbol'] + " " + priceUS['rate'] + " - " + priceUS['description']
-	print "[" + priceGBP['code'] + "] " + priceGBP['symbol'] + " " + priceGBP['rate'] + " - " + priceGBP['description']
-	print "[" + priceEUR['code'] + "] " + priceEUR['symbol'] + " " + priceEUR['rate'] + " - " + priceEUR['description']
+	print("[" + priceUS['code'] + "] " + priceUS['symbol'] + " " 
+		+ priceUS['rate'] + " - " + priceUS['description'])
+	
+	print("[" + priceGBP['code'] + "] " + priceGBP['symbol'] + " "
+		+ priceGBP['rate'] + " - " + priceGBP['description'])
+	
+	print("[" + priceEUR['code'] + "] " + priceEUR['symbol'] + " " 
+		+ priceEUR['rate'] + " - " + priceEUR['description'])
 
 # main function
-country = sys.argv
+country = argv
 if len(country) > 1 and country[1].upper() == "HELP":
 	help()
 	sys.exit(0)
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-data = json.loads(urllib.urlopen(url).read())
-print "Bitcoin price in " + data['time']['updated']
+string_request = str(urlopen(url).read(), encoding='utf-8')
+data = loads(string_request)
+print("Bitcoin price in " + data['time']['updated'])
 if len(country) == 1:
 	default()
 else:
-	for i in range(1, len(country)):
-		get_country(country[i].upper())
+	for country in country[1:]:
+		get_country(country.upper())
