3e01eae5cc7845f5d427ea38033e8834db440aac
ViniciusBiermann <viniciusbiermann@hotmail.com>
2015-10-01 17:00:33 -0300

    Started second practical work

---
+++ prolog/T2/programa.pl
@@ -0,0 +1,75 @@
+/*
+   Programacao Logica - Prof. Alexandre G. Silva - 30set2015
+   
+   RECOMENDACOES:
+   
+   - O nome deste arquivo deve ser 'programa.pl'
+   
+   - O nome do banco de dados deve ser 'desenhos.pl'
+   
+   - Dicas de uso podem ser obtidas na execucação: 
+     ?- menu.
+     
+   - Exemplo de uso:
+     ?- load.
+     ?- searchAll(id1).
+     
+   - Colocar o nome e matricula de cada integrante do grupo
+     nestes comentarios iniciais do programa
+*/
+
+% Apaga os predicados 'xy' da memoria e carrega os desenhos a partir de um arquivo de banco de dados
+load :-
+    retractall(xy(_,_,_)),
+    open('desenhos.pl', read, Stream),
+    repeat,
+        read(Stream, Data),
+        (Data == end_of_file -> true ; assert(Data), fail),
+        !,
+        close(Stream).
+
+% Ponto de deslocamento, se <Id> existente
+new(Id,X,Y) :-
+    xy(Id,_,_),
+    assertz(xy(Id,X,Y)),
+    !.
+
+% Ponto inicial, caso contrario
+new(Id,X,Y) :-
+    asserta(xy(Id,X,Y)),
+    !.
+
+% Exibe opcoes de busca
+search :-
+    write('searchAll(Id).     -> Ponto inicial e todos os deslocamentos de <Id>'), nl,
+    write('searchFirst(Id,N). -> Ponto inicial e os <N-1> primeiros deslocamentos de <Id>'), nl,
+    write('searchLast(Id,N).  -> Lista os <N> ultimos deslocamentos de <Id>').
+
+searchAll(Id) :-
+    listing(xy(Id,_,_)).
+
+% Exibe opcoes de alteracao
+change :-
+    write('change(Id,X,Y,Xnew,Ynew).  -> Altera o ponto inicial de <Id>'), nl,
+    write('changeFirst(Id,Xnew,Ynew). -> Altera o ponto inicial de <Id>'), nl,
+    write('changeLast(Id,Xnew,Ynew).  -> Altera o deslocamento final de <Id>').
+
+% Grava os desenhos da memoria em arquivo
+commit :-
+    open('desenhos.pl', write, Stream),
+    telling(Screen),
+    tell(Stream),
+    listing(xy),
+    tell(Screen),
+    close(Stream).
+
+% Exibe menu principal
+menu :-
+    write('load.        -> Carrega todos os desenhos do banco de dados para a memoria'), nl,
+    write('new(Id,X,Y). -> Insere um deslocamento no desenho com identificador <Id>'), nl,
+    write('                (se primeira insercao, trata-se de um ponto inicial)'), nl,
+    write('search.      -> Consulta pontos dos desenhos'), nl,
+    write('change.      -> Modifica um deslocamento existente do desenho'), nl,
+    write('remove.      -> Remove um determinado deslocamento existente do desenho'), nl,
+    write('undo.        -> Remove o deslocamento inserido mais recentemente'), nl,
+    write('commit.      -> Grava alteracoes de todos dos desenhos no banco de dados').
