db323347e414188e75d4ac459069232de75fdd3d
CaiqueMarques <caiquermarques95@gmail.com>
2015-10-12 17:13:31 -0300

    Remove copyright messages

--- prolog/T1/t1c.pl
+++ prolog/T1/t1c.pl
@@ -1,5 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright (c) 2015 Caique Rodrigues Marques,
+% Créditos: 
+%			Caique Rodrigues Marques,
 %			Gustavo José Carpeggiani,
 %			Vinícius Couto Biermann
 %
