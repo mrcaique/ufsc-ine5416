3825fa1234a3f1963c138a75ecf2951580d747f6
CaiqueMarques <caiquermarques95@gmail.com>
2015-10-12 23:52:13 -0300

    Add searchFirst and searchLast (changeFirst by @ViniciusBiermann)

--- prolog/T2/programa.pl
+++ prolog/T2/programa.pl
@@ -1,21 +1,20 @@
 /*
    Programacao Logica - Prof. Alexandre G. Silva - 30set2015
    
+    Adições sugeridas implementadas por:
+            Caique Rodrigues Marques
+            Gustavo José Carpeggiani
+            Vinícius Couto Biermann
+
    RECOMENDACOES:
-   
    - O nome deste arquivo deve ser 'programa.pl'
-   
    - O nome do banco de dados deve ser 'desenhos.pl'
-   
    - Dicas de uso podem ser obtidas na execucação: 
      ?- menu.
      
    - Exemplo de uso:
      ?- load.
      ?- searchAll(id1).
-     
-   - Colocar o nome e matricula de cada integrante do grupo
-     nestes comentarios iniciais do programa
 */
 
 % Apaga os predicados 'xy' da memoria e carrega os desenhos a partir de um arquivo de banco de dados
@@ -48,6 +47,39 @@ search :-
 searchAll(Id) :-
     listing(xy(Id,_,_)).
 
+% Lista o ponto inicial e os N-1 primeiros pontos.
+% Funções:
+%       nth1(Index, List, Elem)
+%       Supondo uma lista cuja primeira posição é em um.
+%       Dado um número inteiro em Index representando a
+%       posição de um elemento na lista List, este elemento
+%       é armazenado em Elem.
+%
+%       between(Low, High, Value)
+%       Low e High são inteiros, então Low <= High.
+%       Se Value é um inteiro, então a sentença é
+%       verdadeira se Low <= Value <= High.
+searchFirst(Id, N) :-
+    findall(Points, (xy(Id, X, Y), append([Id], [X], ListX), append(ListX, [Y], Points)), All),
+    between(1, N, Middle),
+    nth1(Middle, All, Element),
+    print(Element), nl,
+    false.
+
+% Lista os N últimos deslocamentos de Id.
+% Função:
+%       nth0(Index, List, Elem)
+%       Supondo uma lista cuja primeira posição é em zero.
+%       Mesmo funcionamento de nth1().
+searchLast(Id, N) :-
+    findall(Points, (xy(Id, X, Y), append([Id], [X], ListX), append(ListX, [Y], Points)), All),
+    length(All, Size),
+    Start is Size-N,
+    between(Start, Size, Middle),
+    nth0(Middle, All, Element),
+    print(Element), nl,
+    false.
+
 % Exibe opcoes de alteracao
 change :-
     write('change(Id,X,Y,Xnew,Ynew).  -> Altera o ponto inicial de <Id>'), nl,
@@ -56,6 +88,10 @@ change :-
 
 change(Id, X, Y, Xnew, Ynew) :-
     remove(Id, X, Y),
+    asserta(xy(Id, Xnew, Ynew)), !.
+
+changeFirst(Id, Xnew, Ynew) :-
+    remove(Id, _, _), !,
     asserta(xy(Id, Xnew, Ynew)), !.
 
 % Remove um deslocamento ou ponto
